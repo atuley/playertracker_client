08d1743d64a46eef9cf5f0ddb04417948a2aca74
Alex Tuley <alex.p.tuley@gmail.com>
2019-05-19 19:33:54 -0400

    Add redux dev tools

--- src/index.js
+++ src/index.js
@@ -2,18 +2,19 @@ import React from 'react';
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk';
-import rootReducer from './reducers/rootReducer';
+import rootReducer from './reducers/root_reducer';
 import { createStore, applyMiddleware } from 'redux';
-import Playertracker from './Playertracker'
+import SearchContainer from './containers/search_container';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
-    rootReducer,
+    rootReducer, composeEnhancers(
     applyMiddleware(thunk)
-  );
+  ));
 
 render(
   <Provider store={store}>
-    <Playertracker />
+    <SearchContainer />
   </Provider>,
   document.getElementById('root')
 );
