96037067daf057796d64cbf9566486189980e8c4
Alex Tuley <alex.p.tuley@gmail.com>
2019-06-10 20:42:04 -0400

    Style player search results

--- src/components/search_result.js
+++ src/components/search_result.js
@@ -1,18 +1,23 @@
 import React from 'react'
 
 const SearchResult = ({ player }) => {
-  const { firstName, lastName, number, position } = player
-
+  const { firstName, lastName, number, position, teamColor } = player
+  const teamGradient = {
+    background: `linear-gradient(to right, 
+      rgba(255,0,0,0) 0%,
+      rgba(255,0,0,0) 20%, 
+      ${teamColor} 150%)`
+  }
   return (
-    <tr className="search-result">
-      <td className="search-result__col u-border-left">
+    <tr className="search-result" style={teamGradient}>
+      <td className="search-result__name">
         <span className="u-block">{`${firstName} ${lastName}`}</span>
         <span className="u-block">
           #{number} | {position}
         </span>
       </td>
-      <td className="search-result__col u-border-right">
-        <button className="follow-btn">Follow</button>
+      <td className="search-result__action">
+        <button className="primary-btn">Follow</button>
       </td>
     </tr>
   )
