93f5805c5c0053984f85802035ca2857c4b22503
Alex Tuley <alex.p.tuley@gmail.com>
2019-06-15 13:04:46 -0400

    Always lowercase search query

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,12 @@
 import _ from 'lodash'
 
 export const search = (searchQuery, players) => {
+  const lowercaseQuery = _.toLower(searchQuery)
   return _.filter(players, player =>
     _.chain(player)
       .values()
       .toLower()
-      .includes(searchQuery)
+      .includes(lowercaseQuery)
       .value()
   )
 }
