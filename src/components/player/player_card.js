e173cda2389aaf867dca965d3839c657e13426c4
Alex Tuley <alex.p.tuley@gmail.com>
2019-09-02 19:41:07 -0400

    REAL DATA!!

--- src/components/player/player_card.js
+++ src/components/player/player_card.js
@@ -2,20 +2,38 @@ import React from 'react'
 import { getPlayerImage, hex2rgba } from '../../utils'
 import NameBadge from './name_badge'
 
-const PlayerCard = ({ player }) => (
-  <div
-    className="player-card"
-    style={{ backgroundColor: hex2rgba(player.teamColor, 0.3) }}
-  >
-    <div className="player-card__image-wrapper">
-      <img className="player-card__image" src={getPlayerImage(player.id)} />
+const PlayerCard = ({ player }) => {
+  const {
+    id,
+    points,
+    assists,
+    rebounds,
+    steals,
+    blocks,
+    minutes,
+    teamColor,
+    turnovers
+  } = player
+
+  return (
+    <div
+      className="player-card"
+      style={{ backgroundColor: hex2rgba(teamColor, 0.3) }}
+    >
+      <div className="player-card__image-wrapper">
+        <img className="player-card__image" src={getPlayerImage(id)} />
+      </div>
+      <div className="player-card__stats">
+        <NameBadge {...player} />
+        <h1>
+          {points}PTS {assists}AST {rebounds}REB
+        </h1>
+        <div>
+          {steals}STL {blocks}BLK {turnovers}TO {minutes}MIN
+        </div>
+      </div>
     </div>
-    <div className="player-card__stats">
-      <NameBadge {...player} />
-      <h1>32PTS 8AST 9REB</h1>
-      <div>1STL 1BLK 1TO 10MIN</div>
-    </div>
-  </div>
-)
+  )
+}
 
 export default PlayerCard
